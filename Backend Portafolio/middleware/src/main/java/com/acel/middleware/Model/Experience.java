27cd1f7da380686a82f7cb4385ffc10fcfde67dd
Rama <45132664+rama42111869@users.noreply.github.com>
2022-11-17 02:05:27 -0300

    Delete Backend Portafolio directory

--- Backend Portafolio/middleware/src/main/java/com/acel/middleware/Model/Experience.java
+++
@@ -1,4 +0,0 @@
-package com.acel.middleware.Model;
-
-public class Experience {
-}
--- Backend Portafolio/middleware/src/main/java/com/acel/middleware/Service/Interface/IShoppingCartService.java
+++
@@ -1,9 +0,0 @@
-package com.acel.middleware.Service.Interface;
-import com.acel.middleware.Model.CartProduct;
-import org.springframework.http.ResponseEntity;
-
-public interface IShoppingCartService {
-    ResponseEntity<CartProduct[]> listAllPrFromUs(Long userId);
-    ResponseEntity<Integer> savePrToSc(Long userId, Long productId);
-    ResponseEntity<Integer> deletePrFromSc(Long id);
-}
